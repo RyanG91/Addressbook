224590ddd7616657a7616e28f792658b7a6260ee
Ryan Grosser <ryan.grosser91@gmail.com>
2018-04-17 16:25:35 +1000

    A simple address book app

---
+++ app/models/contact.rb
@@ -0,0 +1,8 @@
+class Contact < ApplicationRecord
+  geocoded_by :full_address   # can also be an IP address
+  after_validation :geocode          # auto-fetch coordinates
+  
+  def full_address
+    return "#{street}, #{city}, #{state}, #{postcode}"
+  end
+end
